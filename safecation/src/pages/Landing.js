e2cceba8f18497baea10294ee634d91a988f7d3e
Jaxon Avena <jaxonavena@gmail.com>
2024-04-12 22:51:25 -0500

    Got an input box up. Needs to actually do stuff though.

--- safecation/src/pages/Landing.js
+++ safecation/src/pages/Landing.js
@@ -1,13 +1,30 @@
-import React, { useState, useLocat } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 
+// Define TextInput component
+const TextInput = () => {
+  return (
+    <div>
+      <label htmlFor="textInput">Enter Address:</label>
+      <input type="text" id="textInput" />
+    </div>
+  );
+}
+
+// Landing component
 export default function Landing() {
+    // Access current location
     let location = useLocation();
     const { pathname } = location;
-    return(
-       <Container>
+
+    return (
+      <Container>
         <h1>Safecation</h1>
-       </Container>
-    )
+        <div>
+          {/* Render TextInput component */}
+          <TextInput />
+        </div>
+      </Container>
+    );
 }
