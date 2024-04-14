7756ea284037b3af69774bceb7190701164b39f3
[petejunge7@gmail.com] <petejunge7@gmail.com>
2024-04-13 21:01:23 -0500

    "added back removed stuff"

--- safecation/src/pages/Landing.js
+++ safecation/src/pages/Landing.js
@@ -276,6 +276,84 @@ const getZipCodesForSquares = async (coords) => {
     }
     
   }
+  useEffect(() => {
+    if (zipcode && zipcode !== 12345) {
+      console.log("grabing crime data")
+      getCrime();
+    }
+    }, [zipcode]);
+  // console.log("zip: ", zipcode)
+
+  const getCrime = async () => {
+
+ 
+    try {
+
+      const response = await fetch(`https://crime-data-by-zipcode-api.p.rapidapi.com/crime_data?zip=${zipcode}`,  {     
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '01836c3714msh582bac2093a6a03p19e6abjsn2b918034e4c0',
+        'X-RapidAPI-Host': 'crime-data-by-zipcode-api.p.rapidapi.com'
+      }
+      });
+
+      const data = await response.json();
+
+      const breakdownList = [];
+      // console.log(data);
+
+      // Iterate over the Crime Breakdown array
+      const overallData = data['Overall'];
+
+      Object.entries(overallData).forEach(([key, value]) => {
+        const categoryString = `${key}: ${value}`;
+        breakdownList.push(categoryString);
+      });
+      
+      data['Crime BreakDown'].forEach(crimeCategory => {
+        Object.entries(crimeCategory).forEach(([key,value]) => {
+          
+         
+          if (key != "0"){
+          const categoryString = `${key}: ${Object.entries(value).map(([crimeType, rate]) => `${crimeType}: ${rate}`).join(', ')}`;
+          // Add the constructed string to the list
+          breakdownList.push(categoryString);
+          } else if (key === "0"){ 
+            const categoryString = `${value}`
+          }
+
+      });
+        console.log(crimeCategory);
+        // Extract the crime category and its rates
+        
+        const category = Object.keys(crimeCategory)[0];
+        // const subrates = Object.keys(crimeCategory)[1]
+        const rates = crimeCategory[category]; 
+
+        // Construct a string representation of the crime category and rates
+        const categoryString = `${category}: ${Object.entries(rates).map(([crimeType, rate]) => `${crimeType}: ${rate}`).join(', ')}`;
+
+        // Add the constructed string to the list
+        breakdownList.push(categoryString);
+      });
+
+      // Set the crime breakdown list in the state
+      setcrimeData(breakdownList);
+      console.log("breakdown list: ",breakdownList)
+      
+      // const result = await response.text();
+      // console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+  // useEffect(() => {
+  //   if (crimeData && crimeData !== [] ) {
+  //     console.log("crimeData: " );
+  //     console.log(crimeData);
+  //   }
+  //   }, [crimeData]);
   let requestData = [latitude, longitude];
 
   const responseData = Disease({ requestData });
